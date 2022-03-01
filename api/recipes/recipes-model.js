e37cb7c6031bbcc4909ebe308670da4f6bfb7636
jacobjolibois <jacobjolibois@hey.com>
2022-03-01 12:17:08 -0600

    cleaned up the returned recipe object

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -19,35 +19,35 @@ async function getById(recipe_id) {
 	const getIngredients = await db('recipes as r')
 		.join('recipe_ingredients as ri', 'ri.recipe_id', 'r.recipe_id')
 		.join('ingredients as i', 'i.ingredient_id', 'ri.ingredient_id')
+		.join('categories as c', 'c.cat_id', 'r.cat_id')
 		.where('r.recipe_id', recipe_id)
-		.select('r.recipe_id', 'recipe_title', 'recipe_source', 'i.ingredient_id', 'ingredient_name', 'quantity');
+		.select('r.recipe_id', 'recipe_title', 'recipe_source', 'cat_name', 'i.ingredient_id', 'ingredient_name', 'quantity');
 	
-		const getSteps = await db('recipes as r')
+	const getSteps = await db('recipes as r')
 		.join('steps as s', 's.recipe_id', 'r.recipe_id')
 		.where('r.recipe_id', recipe_id)
 		.select('step_id', 'step_number', 'step_instructions');
 
 	const recipe = {
 		recipe_id: getIngredients[0].recipe_id,
-		recipe_title: getIngredients[0].recipe_title,
-		recipe_source: getIngredients[0].recipe_source,
+		title: getIngredients[0].recipe_title,
+		source: getIngredients[0].recipe_source,
+		category: getIngredients[0].cat_name,
 		ingredients: [],
 		instructions: []
 	};
 
 	for (let i of getIngredients) {
 		recipe.ingredients.push({
-			ingredient_id: i.ingredient_id,
-			ingredient_name: i.ingredient_name,
+			ingredient: i.ingredient_name,
 			quantity: i.quantity
 		});
 	}
 	
 	for (let s of getSteps) {
 		recipe.instructions.push({
-			step_id: s.step_id,
-			step_number: s.step_number,
-			step_instructions: s.step_instructions
+			step: s.step_number,
+			instructions: s.step_instructions
 		});
 	}
 
